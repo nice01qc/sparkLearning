d9969bdc117514676322c0aecc47e04172cbfb87
Li Jinhao <nice01qc@163.com>
2020-11-08 10:32:59 +0800

    init

---
+++ src/main/java/com/ljh/SparkSessionTemplate.scala
@@ -0,0 +1,28 @@
+package com.ljh
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+/**
+ * 一个 Spark Session 使用例子
+ */
+
+object SparkSessionTemplate {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("TmpTest").setMaster("local[2]")
+
+    val spark = SparkSession.
+      builder().
+      appName("test-1").
+      config(sparkConf).
+      getOrCreate()
+
+    val myRange = spark.range(10000).toDF("number")
+
+    myRange.rdd.saveAsTextFile("/home/work/data/test/spark_dir")
+
+    println("成功。。。。")
+  }
+
+}
